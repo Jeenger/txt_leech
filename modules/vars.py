2a69ba7550d32da887e579cb4743b22e4ccc53ca
Jeenger <162483797+Jeenger@users.noreply.github.com>
2024-03-06 22:33:45 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "18184376"
+api_hash = "76e01466830c68c9ccf8e00e54786073"
+bot_token = "7121547572:AAHWMo5Hl-YDio_tN3lljzQHpcZrR83P_mw"
